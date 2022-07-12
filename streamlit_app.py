978f4428307bd6e23a859593b7537c851778e2b3
Nufu <94199571+sfc-gh-nrajaofetra@users.noreply.github.com>
2022-07-12 23:50:26 +0200

    Update streamlit_app.py
    ajout détail du menu Petit Déjeuner

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 
 import streamlit
 streamlit.title('My parents New Healthy Diner')
+streamlit.header('Breakfast Menu - Petit Dejeuner')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spnach & Rocket Smoothie')
+streamit.text('Hard-Boiled Free-Range Egg')
