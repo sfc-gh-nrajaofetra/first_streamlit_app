406906e879ffc44a395e96f2022c6b5354a1a700
Nufu <94199571+sfc-gh-nrajaofetra@users.noreply.github.com>
2022-07-12 23:54:36 +0200

    Update streamlit_app.py
    changement menu et ajout emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 
 import streamlit
 streamlit.title('My parents New Healthy Diner')
-streamlit.header('Breakfast Menu - Petit Dejeuner')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spnach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favorites  - Petit Dejeuner')
+streamlit.text('🫕 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🍵 Kale, Spnach & Rocket Smoothie')
+streamlit.text('🥚 Hard-Boiled Free-Range Egg')
+streamlit.text('🥒 🍞 Cuncumber Toast')
